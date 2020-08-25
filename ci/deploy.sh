507f23a0996724812e2adfd49e5f23b4a1a158f8
CaioRuizz <caio.ruiz00@gmail.com>
2020-08-25 14:16:08 -0300

    test env vars

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -e
 
-# cd react-app
+S3_URL=s3://www.ci-test.com
+echo $S3_URL
+
+cd react-app
 # npm run build
-aws --version
+
+# aws s3 cp build $S3_URL --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
+
 echo var PWD
 echo $PWD
