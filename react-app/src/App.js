45188441046605a8b6cdaa028cab742b7d14da43
CaioRuizz <caio.ruiz00@gmail.com>
2020-08-26 14:05:25 -0300

    test deploy

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          Test success
+          Garrafinha
         </p>
         <a
           className="App-link"
