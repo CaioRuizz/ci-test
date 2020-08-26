5c28f30988bbcecacf7a54aad3f3b3d9c6b50aee
CaioRuizz <caio.ruiz00@gmail.com>
2020-08-26 11:46:17 -0300

    test success

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          Test success
         </p>
         <a
           className="App-link"
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test fail
+          Learn React
         </a>
       </header>
     </div>
