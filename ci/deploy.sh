0e2f613a26ecd14a591cc09f1fe8c1073489938a
CaioRuizz <caio.ruiz00@gmail.com>
2020-08-26 11:37:34 -0300

    test aws credentials on secret

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,6 +5,7 @@ ORIGINAL_DIR=$PWD
 S3_URL=s3://www.ci-test.com
 
 cd react-app
+npm run build
 
 aws s3 cp build $S3_URL --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
 
